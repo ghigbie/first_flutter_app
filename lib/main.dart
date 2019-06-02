77354c8bae6a89fbaab5d5d1405278fcb728fd16
ghigbie <gehigbie@gmail.com>
2019-06-01 19:28:50 -0600

    Build app

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,52 @@ class MooApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       theme:  ThemeData(
-        primarySwatch: Colors.blue
+        primarySwatch: Colors.black
       ),
+      home: HomePage(title: 'Welcome to Moo App!')
     );
+  }
+}
+
+class HomePage extends StatefulWidget{
+  HomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  var color = Colors.white;
+  void _changeColor(){
+    setState(() {
+      if (color == Colors.white){
+        color = Colors.blue;
+      }else{
+        color = Colors.white;
+      }
+    });
+  }
+  
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Moo Flutter!!!',
+              style: TextStyle(fontSize: 50, color: color)
+            ),
+          ]
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _changeColor,
+        child: Icon(Icons.airplanemode_active
+      )
+    )
+  );
   }
 }
